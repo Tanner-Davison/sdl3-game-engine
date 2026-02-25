3dd8f5d162d55b6d63967dd68f5187a5d65b1505
Tanner Davison <tanner.davison95@gmail.com>
2026-02-24 21:02:50 -0700

    grouped script together

--- generate_docs.sh
+++ generate_docs.sh
@@ -2,3 +2,12 @@
 echo "Generating Documentation..."
 doxygen MyDoxyFile
 echo "Done! Open docs/html/index.html"
+
+if [ ! -f "docs/html/index.html" ]; then
+    echo "Documentation not found! Generating..."
+    doxygen
+fi
+
+echo "Opening File Documentation in Browser using wslview"
+wslview docs/html/index.html
+echo "Done! Viewing Documentation in Browser!"
--- view_documentation.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ ! -f "docs/html/index.html" ]; then
-    echo "Documentation not found! Generating..."
-    doxygen
-fi
-
-echo "Opening File Documentation in Browser using wslview"
-wslview docs/html/index.html
-echo "Done! Viewing Documentation in Browser!"
