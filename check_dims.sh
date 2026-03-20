112886d7517f9828da55a46b4faa06f5a8f027ef
Tanner Davison <tanner.davison95@gmail.com>
2026-03-19 22:09:12 -0600

    cleaned up OLD scripting

--- check_dims.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /Users/tanner.davison/projects/cpp/sdl-sandbox
-python3 check_dims.py game_assets/backgrounds/2.png game_assets/backgrounds/1.png game_assets/backgrounds/3.png game_assets/backgrounds/4.png game_assets/backgrounds/5.png game_assets/backgrounds/deepspace_scene.png game_assets/backgrounds/battleground.png game_assets/backgrounds/bg_castle.png 2> /dev/null
--- run_dims.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /Users/tanner.davison/projects/cpp/sdl-sandbox
-c++ -std=c++17 -o pngdims pngdims.cpp 2>&1 && ./pngdims \
-  game_assets/backgrounds/*.png \
-  2>&1
-
